6d287f442ce26542ce3e35e6abc20c2877d0d89a
omar7altawil <omar.altawil@hotmail.com>
2019-06-17 09:00:58 +0200

    funaction change name

--- pymer.py
+++ pymer.py
@@ -1,9 +1,9 @@
 import time
 
-def starttimer():
+def startTimer():
      global pymerT1 
      pymerT1 =time.time()
 
-def endtime():
+def endTime():
     print(str((time.time()-pymerT1)*1000)+ "ms")
 
